d9be5e5a1bece3471dad34be32ee757a379e8f6d
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2023-06-13 00:28:43 +0500

    поправил ссылки

--- site.config.ts
+++ site.config.ts
@@ -45,7 +45,18 @@ export default siteConfig({
     '/about': 'd4285e9b710d4232934203597f8d863d',
     '/dev': 'd209ba7248c7478a9d228c79fb123639',
     '/graphdesign': '64c1cd58cd504c9c8e4e2c7f752ac111',
-    '/': '#6dbad34251864bc7b3d6f4f6423ccb10'
+    '/': '#6dbad34251864bc7b3d6f4f6423ccb10',
+    '/dev-myap': '08fba823beb347829cac64a7fe12fd54',
+    '/dev-b4d': 'abe45dc61a774017a7f7f8e851627f3c',
+    '/dev-catenergy': 'cat-energy-c88997393eb4431790cd3b1e8246c303',
+    '/dev-hungry': 'hungry-012be175fdd942e394c4c05c99574ec7',
+    '/dev-centerlogistic': 'center-logistic-f8583effaf764f3d9c9f916a4f2d0921',
+    '/dev-laslesvpn': 'laslesvpn-30cb2944e45d433dbdddb6f35768d5af',
+    '/dev-techgear': 'techgear-957a266a42ea4038856318570343082b',
+    '/dev-bouncer': 'bouncer-d99319538baa4625aa19069e833213e9',
+    '/dev-servion': 'servion-c94b3959d2bd43448af69a60364ad56d',
+    '/dev-firecamp': 'firecamp-5049a1ca8f8c49ecb4d42a70f69404ac',
+    '/dev-inteo': 'inteo-9cd4eb809277457385073561339fcef3'
   },
 
   // whether to use the default notion navigation style or a custom one with links to
